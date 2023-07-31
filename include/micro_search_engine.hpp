a116d265af3ad251511ae744eee6a557cebe554e
SamNgigi <sam.t.ngigi@gmail.com>
2023-07-31 23:40:43 +0300

    [feat]: Adds read from file functionality

--- include/micro_search_engine.hpp
+++ include/micro_search_engine.hpp
@@ -2,6 +2,6 @@
 #define MICRO_SEARCH_ENGINE_HPP
 
 #include <iostream>
-
+#include <cstring>
 
 #endif //MICRO_SEARCH_ENGINE_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
+#include <memory>
+#include <fstream>
 
 #include "micro_search_engine.hpp"
 
 int main(int argc, char **argv){
     std::cout << "Hello World!" << std::endl;
 
-    if(argc!=5)
+    if(argc!=5 || std::strcmp(argv[1], "-d") || std::strcmp(argv[3], "-k")){
         std::cout << "Wrong Arguments" << std::endl;
-    else 
-        std::cout << "Correct Arguments" << std::endl;   
+        return -1;
+    }
+    std::cout << "Please Wait..." << std::endl;
+    int linecounter = 0;
+    int maxLength = -1;
+    int k = std::atoi(argv[4]);
+    
+    std::fstream file;
+    file.open(argv[2], std::ios::in); // open in read mode
+    if(!file) std::cerr << "Error opening file" << std::endl; 
+    file.close();
     return 0;
 }
