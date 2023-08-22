4447bf91281f861866f18a87dc0912aad9be3c4e
SamNgigi <sam.t.ngigi@gmail.com>
2023-08-22 20:30:19 +0300

    [misc]: Updates README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+# Building A Search Engine In Modern C++ - (WIP)
+
+A modern c++ implementation of a search engine guided by [this](https://www.youtube.com/watch?v=4k29hrwkBoI) resource.
+
+This implementation is on a windows 11 using MSVC 19 compiler.
--- src/MyMap.cpp
+++ src/MyMap.cpp
@@ -1,19 +1,5 @@
 #include "MyMap.hpp"
 
-
-/* int MyMap::insert(const std::string &line, int i){
-    std::istringstream ss(line);
-    int current;
-    std::string token;
-    ss >> current;
-    if(current != i) return -1;
-    std::getline(ss, token, '\n');
-    token.erase(0, token.find_first_not_of(' ')); // Trim leading spaces;
-    token.erase(0, token.find_last_not_of(' ')+1); // Trim trailing spaces;
-    documents[i] = token;
-    return -1;
-} */
-
 int MyMap::insert(const std::string& line, int i) {
     int curr;
     std::string token = line.substr(0, line.find("\t"));
