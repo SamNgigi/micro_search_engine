e08762bbf0db538e91c852bd4e6c3cc8fa12f39c
SamNgigi <sam.t.ngigi@gmail.com>
2023-08-22 19:34:43 +0300

    [feat]: Updates MyMap to use string vector & int vector for documents and lengths respectively

--- include/MyMap.hpp
+++ include/MyMap.hpp
@@ -2,24 +2,29 @@
 #define MYMAP_H
 
 #include <iostream>
+#include <sstream>
+#include <fstream>
+#include <cstring>
 #include <string>
 #include <vector>
 #include <memory>
 
 class MyMap{
-    int size; // number of documents
-    int buffersize; // length of the biggest document;
+    // int size; // number of documents
+    // int buffersize; // length of the biggest document;
     std::vector<std::string> documents; // each document
-    std::unique_ptr<int> lengths; // length of each map
+    std::vector<int> lengths; // length of each map
     public:
-        MyMap(int size, int buffersize) : size(size), buffersize(buffersize){
+        /* MyMap(int size, int buffersize) : size(size), buffersize(buffersize){
             documents.resize(size);
             lengths = std::make_unique<int>();
             for(int i=0; i<size; i++) documents[i] = std::string(buffersize, '\0');
-        }
-        int insert(std::string line, int i);
-        const int getsize(){return size;}
-        const int getbuffersize(){return buffersize; }
-        ~MyMap(){}
+        } */
+        MyMap(int size): documents(size), lengths(size){}
+        int insert(const std::string &line, int i);
+        void setLengths(int id, int length){lengths[id] = length;}
+        int getLengths(int id){return lengths[id];}
+        void print(int i){std::cout << " " << documents[i] << "\n";}
+        const int getsize(){return documents.size();}
 };
 #endif // MYMAP_H
--- src/MyMap.cpp
+++ src/MyMap.cpp
@@ -2,6 +2,14 @@
 
 
 int MyMap::insert(std::string line, int i){
-    
-    return 1;
+    std::istringstream ss(line);
+    int current;
+    std::string token;
+    ss >> current;
+    if(current != i) return -1;
+    std::getline(ss, token, '\n');
+    token.erase(0, token.find_first_not_of(' ')); // Trim leading spaces;
+    token.erase(0, token.find_last_not_of(' ')+1); // Trim trailing spaces;
+    documents[i] = token;
+    return -1;
 }
